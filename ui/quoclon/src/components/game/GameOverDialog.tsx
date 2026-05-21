20eec75254fc9e012f0410c6cf4acf7cac4e9957
stan <jstaneck@outlook.com>
2026-05-21 11:33:15 -0300

    move game over dialog to its own file

---
+++ ui/quoclon/src/components/game/GameOverDialog.tsx
@@ -0,0 +1,39 @@
+import { useContext, useEffect, useRef } from "react";
+import { getGameOverText } from "./player.type";
+import { LobbyContext, PlayerContext } from "../../App.tsx";
+import "./gameoverdialog.css"
+
+interface Props {
+    winnerPlayerName: string,
+}
+
+export const GameOverDialog = ({winnerPlayerName}: Props) => {
+    const dialogRef = useRef<HTMLDialogElement>(null);
+    const lobbyContext = useContext(LobbyContext);
+    const playerContext = useContext(PlayerContext);
+
+    
+    useEffect(() => {
+        if (lobbyContext.winnerPlayerId !== undefined) {
+            onOpenModal();
+        }
+    }, [lobbyContext.winnerPlayerId])
+
+    const onOpenModal = () => {
+        dialogRef.current?.showModal();
+    }
+
+
+    const onCloseModal = () => {
+        dialogRef.current?.close();
+    }
+    
+    return (
+        <div className="dialog-container">
+            <dialog ref={dialogRef} className="game-over-dialog" closedby="any">
+                <p>{getGameOverText(winnerPlayerName, lobbyContext.winnerPlayerId === playerContext.id)}</p>
+                <button onClick={onCloseModal}>Cerrar</button>
+            </dialog>        
+        </div>
+    )
+}
--- ui/quoclon/src/components/game/Gameframe.tsx
+++ ui/quoclon/src/components/game/Gameframe.tsx
@@ -4,29 +4,13 @@ import { getPlayerById, type GameState } from "../../game/GameState"
 import { requestPlayerMove, requestWallPlacement } from "../../server/server"
 import { translateGridPositionToClient, translateWallsToClient } from "../../server/utils"
 import { Board } from "../board/Board"
-import { useContext, useEffect, useRef } from "react"
-import { LobbyContext, PlayerContext } from "../../App.tsx"
-import { getGameOverText } from "./player.type.ts"
+import { GameOverDialog } from "./GameOverDialog.tsx"
+import { useContext } from "react"
+import { LobbyContext } from "../../App.tsx"
 
 
 export const GameFrame = ({ gameState }: { gameState: GameState }) => {
-    const dialogRef = useRef<HTMLDialogElement>(null);
     const lobbyContext = useContext(LobbyContext);
-    const playerContext = useContext(PlayerContext);
-
-    useEffect(() => {
-        if (lobbyContext.winnerPlayerId !== undefined) {
-            onOpenModal();
-        }
-    }, [lobbyContext.winnerPlayerId])
-
-    const onCloseModal = () => {
-        dialogRef.current?.close();
-    }
-    
-    const onOpenModal = () => {
-        dialogRef.current?.showModal();
-    }
 
     const p1Position = translateGridPositionToClient(gameState.playerOne.position.row, gameState.playerOne.position.col);
     const p2Position = translateGridPositionToClient(gameState.playerTwo.position.row, gameState.playerTwo.position.col);
@@ -56,12 +40,7 @@ export const GameFrame = ({ gameState }: { gameState: GameState }) => {
                     currentTurnPlayerId={gameState.currentTurnPlayerId}
             />
             <WallPicker walls={gameState.playerTwo.wallsRemaining} position="bottom"/>
-            <div className="dialog-container">
-                <dialog ref={dialogRef} className="game-over-dialog" closedby="any">
-                    <p>{getGameOverText(winnerPlayerName, lobbyContext.winnerPlayerId === playerContext.id)}</p>
-                    <button onClick={onCloseModal}>Cerrar</button>
-                </dialog>        
-            </div>
+            <GameOverDialog winnerPlayerName={winnerPlayerName}/>
         </div>
     )
 }
