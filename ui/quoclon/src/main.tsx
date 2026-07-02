805c5b57a51f12e9458f8e61c1f330957f4468bb
stan <jstaneck@outlook.com>
2026-07-02 10:53:50 -0300

    add color scheme, and color the body

--- ui/quoclon/src/main.tsx
+++ ui/quoclon/src/main.tsx
@@ -3,17 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Home } from './components/home/home.tsx'
+import './style/color.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        
-        <Route index path="/" element={<Home />} />
-        <Route path="game/:id" element={<App />} />
-        <Route path="about" element={<div>About!!!</div>} />
+    <div className="body-container">
+      
+      <BrowserRouter>
+        <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route path="game/:id" element={<App />} />
+            <Route path="about" element={<div>About!!!</div>} />
+        </Routes>
+      </BrowserRouter>
 
-      </Routes>
-    </BrowserRouter>
+    </div>
   </StrictMode>,
 )
---
+++ ui/quoclon/src/style/color.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+    darkBrown1 : "1E1702",
+    darkBrown2 : "342005",
+
+    softGray1 : "8C886B",
+    softGray2 : "E5E3E4",
+
+    emerald: "047C58"
+}
