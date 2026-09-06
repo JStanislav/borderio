77394f2eeb2209b47593e3354322cec146a378c4
stan <jstaneck@outlook.com>
2026-09-05 21:18:29 -0300

    add cors to env config

--- server/config/config.go
+++ server/config/config.go
@@ -5,10 +5,20 @@ import "github.com/kelseyhightower/envconfig"
 type Config struct {
 	Port                 string `default:"8080"`
 	TimeoutAfterGameOver int    `default:"60" split_words:"true"` // in seconds
+
+	Cors Cors
+}
+
+type Cors struct {
+	AllowedOrigins []string `required:"true" split_words:"true"`
+	AllowedMethods []string `default:"GET,POST,PUT,PATCH,DELETE,OPTIONS" split_words:"true"`
 }
 
 func LoadConfig() Config {
 	var config Config
-	envconfig.Process("", &config)
+	if err := envconfig.Process("", &config); err != nil {
+		panic(err)
+	}
+
 	return config
 }
