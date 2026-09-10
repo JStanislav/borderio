376a3b9b746c7833b67ada8d3af3b53729ae441b
stan <jstaneck@outlook.com>
2026-09-10 16:49:24 -0300

    change ui node env

--- ui/quoclon/config/config.ts
+++ ui/quoclon/config/config.ts
@@ -16,7 +16,7 @@ const LoadOrDefaultValue = (key: string, defaultValue: string): string => {
 };
 
 export const config: Config = {
-    protocol: LoadOrDefaultValue("NODE_ENV", "development") === "production" ? "https://" : "http://",
+    protocol: LoadOrDefaultValue("VITE_NODE_ENV", "development") === "production" ? "https://" : "http://",
     serverUrl: LoadOrDefaultValue("VITE_SERVER_URL", "localhost:8080"),
     port: parseInt(LoadOrDefaultValue("VITE_APP_PORT", "5173"))
 }
